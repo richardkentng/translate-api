c371487404d478ecb0bd06f96c3cf382d0c72afb
Richard Ng <richardkentng@gmail.com>
2022-02-11 17:05:06 -0800

    query selects form and input for English and Chinese

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 console.log("sanity check");
+
+const englishForm = document.body.querySelector(".english-form");
+const englishInput = document.body.querySelector("input[data-sl='en']");
+const chineseForm = document.body.querySelector(".chinese-form");
+const chineseInput = document.body.querySelector("input[data-sl='zh-CH']");
