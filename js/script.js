76f7db75429fd67f4f0d8d3c11b9da9eaa296c6e
Richard Ng <richardkentng@gmail.com>
2022-02-11 17:08:08 -0800

    connects forms with event listener

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ const englishForm = document.body.querySelector(".english-form");
 const englishInput = document.body.querySelector("input[data-sl='en']");
 const chineseForm = document.body.querySelector(".chinese-form");
 const chineseInput = document.body.querySelector("input[data-sl='zh-CH']");
+
+englishForm.addEventListener("submit", translate);
+chineseForm.addEventListener("submit", translate);
+
+function translate(e) {
+  e.preventDefault();
+}
